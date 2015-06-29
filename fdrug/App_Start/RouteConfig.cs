0f9feecb495cf9597914d68778f0d2a9d6113558
shaunmroberts1230 <shaunmroberts1230@gmail.com>
2015-06-28 21:38:48 -0400

    Initial commit

---
+++ fdrug/App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace fdrug
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Home",
+                url: "{action}/{id}",
+                defaults: new { controller = "home", action = "index", id = UrlParameter.Optional }
+                );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "home", action = "index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ fdrug/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(fdrug.Startup))]
+namespace fdrug
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
